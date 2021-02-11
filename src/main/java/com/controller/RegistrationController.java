0fb544a5530b16f5bd7dd09959c44ccfb96b951b
Yaroslav <tangut1998@gmail.com>
2021-02-11 18:23:37 +0400

    tried to fix bug in deploy

--- src/main/java/com/controller/RegistrationController.java
+++ src/main/java/com/controller/RegistrationController.java
@@ -5,6 +5,7 @@ import com.domain.User;
 import com.repos.UserRepo;
 import com.service.UserSevice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,9 @@ public class RegistrationController {
 
     @Autowired
     private UserSevice userSevice;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @GetMapping("/registration")
     public String registration() {
